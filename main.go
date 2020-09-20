41aeb018f0d7a394ff88dfb84d524cc97e6ed004
Ashish Juyal <ashish@bhaifi.com>
2020-09-20 14:57:06 +0530

    XML Encoding

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 func main() {
@@ -34,7 +35,11 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{"Rob", "New Delhi", "110075"},
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(customers)
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
