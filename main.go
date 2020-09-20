72e8ed9d5f85f2e233909c45599d7bca49d82e9a
Ashish Juyal <ashish@bhaifi.com>
2020-09-20 14:55:24 +0530

    JSON Encoding

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 
-	//define routes
+	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
-	//starting server
+	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"Rob", "New Delhi", "110075"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
